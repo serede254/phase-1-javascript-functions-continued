4926665008d80b2bf10ca0f08536101ba0a67363
root <root@SURFACE.>
2024-03-19 13:21:52 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(action="go to the office"){
+    return `This Monday, I will ${action}.`
+}
+function wrapAdjective(outer="*"){
+    return function (inner="special"){
+    return `You are ${outer}${inner}${outer}!`;
+}
+}
